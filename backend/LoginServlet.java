7e33939454602a67fc60527adb36b3be21cfa733
Sarthak Sonawane <sarthaksonawane112@gmail.com>
2025-12-15 11:44:09 +0530

    Update LoginServlet.java

--- backend/LoginServlet.java
+++ backend/LoginServlet.java
@@ -1,1 +1,31 @@
+import java.io.IOException;
+import java.sql.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = PasswordHashUtil.hashPassword(request.getParameter("password"));
+
+        try (Connection con = DBConnection.getConnection()) {
+            String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                response.getWriter().println("Login successful");
+            } else {
+                response.getWriter().println("Invalid credentials");
+            }
+        } catch (Exception e) {
+            response.getWriter().println("Error: " + e.getMessage());
+        }
+    }
+}
 
